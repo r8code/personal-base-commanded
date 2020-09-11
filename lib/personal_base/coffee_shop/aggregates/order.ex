2acde98afcbdaafbec9db50632f405f1189a2818
r8code <r8code@gmail.com>
2020-09-11 17:41:59 +0700

    correcting

--- lib/personal_base/coffee_shop/aggregates/order.ex
+++ lib/personal_base/coffee_shop/aggregates/order.ex
@@ -2,8 +2,8 @@ defmodule PersonalBase.CoffeeShop.Aggregates.Order do
   defstruct [:order_id, :for, status: :initialized]
 
   alias __MODULE__, as: Order
-  alias PersonalBase.Commands.OpenOrder
-  alias PersonalBase.Events.OrderOpened
+  alias PersonalBase.CoffeeShop.Commands.OpenOrder
+  alias PersonalBase.CoffeeShop.Events.OrderOpened
 
   def execute(%Order{order_id: nil}, %OpenOrder{} = command) do
     %OrderOpened{
